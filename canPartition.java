f7a660d251c8939e708fbbd7afd078a1d2367235
wilburliu <wilburliu1123@gmail.com>
2020-10-18 17:29:16 -0500

    416

---
+++ canPartition.java
@@ -0,0 +1,23 @@
+public class canPartition {
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum /= 2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = sum; j >= nums[i-1]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i-1]];
+            }
+        }
+        return dp[sum];
+    }
+}
