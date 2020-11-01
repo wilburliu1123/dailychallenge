f7587e2a288dcce4269bd3d7cca649c872f5ab87
wilburliu <wilburliu1123@gmail.com>
2020-10-31 21:33:29 -0500

    stonyGame3

---
+++ stonyGame3.java
@@ -0,0 +1,16 @@
+public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int take = 0;
+            dp[i] = Integer.MIN_VALUE;
+            for (int j = i; j < Math.min(n, i + 3); j++) {
+                take += stoneValue[j];
+                dp[i] = Math.max(dp[i], take - dp[j + 1]);
+            }
+        }
+        int diff = dp[0];
+        if (diff > 0) return "Alice";
+        if (diff < 0) return "Bob";
+        return "Tie";
+}
