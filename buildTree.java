e364869276cd72600895cfb2b032751a1744a805
wilburliu <wilburliu1123@gmail.com>
2020-11-23 09:52:23 -0600

    build tree pre and post order

--- buildTree.java
+++ buildTree.java
@@ -1,10 +1,11 @@
-int inIndex, postIndex;
-public TreeNode buildTree(int[] inorder, int[] postorder) {
+class buildTree_postorder {
+    int inIndex, postIndex;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
         inIndex = inorder.length - 1;
         postIndex = postorder.length - 1;
-        return buildTree(inorder, postorder, null);
-}
-private TreeNode buildTree(int[] inorder, int[] postorder, TreeNode root) {
+        return postBuildTree(inorder, postorder, null);
+    }
+    private TreeNode postBuildTree(int[] inorder, int[] postorder, TreeNode root) {
         if (postIndex < 0) {
             return null;
         }
@@ -19,4 +20,50 @@ private TreeNode buildTree(int[] inorder, int[] postorder, TreeNode root) {
             n.left = buildTree(inorder, postorder, root);
         }
         return n;
+    }
+}
+class buildTree_preorder {
+    int preIndex, inIndex;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preIndex = 0;
+        inIndex = 0;
+        return buildTree(preorder, inorder, null);
+    }
+
+    private TreeNode buildTree(int[] preorder, int[] inorder, TreeNode root) {
+        if (preIndex > preorder.length - 1) {
+            return null;
+        }
+        TreeNode n = new TreeNode(preorder[preIndex++]);
+        if (n.val != inorder[inIndex]) {
+            n.left = buildTree(preorder, inorder, n);
+        }
+        inIndex++;
+        if (root == null || root.val != inorder[inIndex]) {
+            n.right = buildTree(preorder, inorder, root);
+        }
+        return n;
+    }
+}
+class buildTree_prePostorder {
+    int preIndex, postIndex;
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        preIndex = 0; postIndex = 0;
+        return buildTree(pre, post, null);
+    }
+    private TreeNode buildTree(int[] pre, int[] post, TreeNode root) {
+        if (preIndex > pre.length - 1) {
+            return null;
+        }
+        TreeNode n = new TreeNode(pre[preIndex++]);
+        if (n.val != post[postIndex]) {
+            n.left = buildTree(pre, post, n);
+        }
+        if (n.val != post[postIndex]) {
+            n.right = buildTree(pre, post, n);
+        }
+        postIndex++;
+        return n;
+    }
 }
