fc8534148b379985341c1bbcf8e1528d31f423fb
wilburliu <wilburliu1123@gmail.com>
2020-11-23 17:45:56 -0600

    next Greater element

---
+++ nextGreaterElement.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            int start = map.get(nums1[i]);
+            for (int j = start+1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) {
+                    res[i] = nums2[j];
+                    break;
+                }
+            }
+            if (res[i] == 0) {
+                res[i] = -1;
+            }
+        }
+        return res;
+    }
+}
