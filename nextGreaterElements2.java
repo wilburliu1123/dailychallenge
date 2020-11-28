7f4fd19e6f53a3ea9aec0a645db00199c62b854f
wilburliu <wilburliu1123@gmail.com>
2020-11-28 12:30:22 -0600

    redo nextGreaterElements2

--- nextGreaterElements2.java
+++ nextGreaterElements2.java
@@ -4,6 +4,7 @@ class nextGreaterElements2 {
         Arrays.fill(res, -1);
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < n * 2; i++) {
+            // monotonous increasing stack to store the index of nums
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n])
                 res[stack.pop()] = nums[i % n];
             stack.push(i % n);
