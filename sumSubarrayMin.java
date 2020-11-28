06102d6530c5919a9015eccf43fc369836de974b
wilburliu <wilburliu1123@gmail.com>
2020-11-28 08:27:56 -0600

    commented

--- sumSubarrayMin.java
+++ sumSubarrayMin.java
@@ -3,7 +3,7 @@ class sumSubarrayMin {
         int res = 0, n = arr.length, mod = (int)1e9 + 7;
         int[] left = new int[n], right = new int[n];
         ArrayDeque<int[]> s1 = new ArrayDeque<>(), s2 = new ArrayDeque<>();
-        // mono decrease stack (count how many previous greater element is there)
+        // mono increase stack (count how many previous greater element is there)
         for (int i = 0; i < n; i++) {
             int count = 1;
             while (!s1.isEmpty() && s1.peek()[0] > arr[i])
