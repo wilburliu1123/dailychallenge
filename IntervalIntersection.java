b478a96b36b29ad62304ce5c8835553f522e72b3
Wilbur Liu <wilburliu1123@gmail.com>
2021-03-16 10:54:53 -0500

    interval intersection

---
+++ IntervalIntersection.java
@@ -0,0 +1,23 @@
+class IntervalIntersection {
+  public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+    List<int[]> list = new ArrayList<>();
+    int i = 0, j = 0;
+    while (i < firstList.length && j < secondList.length) {
+      int start = Math.max(firstList[i][0], secondList[j][0]);
+      int end = Math.min(firstList[i][1], secondList[j][1]);
+      if (start <= end) {
+        list.add(new int[] {start, end});
+      }
+      if (firstList[i][1] < secondList[j][1]) {
+        i++;
+      } else {
+        j++;
+      }
+    }
+    int[][] res = new int[list.size()][2];
+    for (int k = 0; k < list.size(); k++) {
+      res[k] = list.get(k);
+    }
+    return res;
+  }
+}
