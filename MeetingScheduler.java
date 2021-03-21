71abb4c78cc67c37a474c4cd05bd9db35bddf75e
Wilbur Liu <wilburliu1123@gmail.com>
2021-03-21 08:25:10 -0500

    meeting scheduler

---
+++ MeetingScheduler.java
@@ -0,0 +1,41 @@
+class MeetingScheduler {
+  public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+    Arrays.sort(slots1, (a,b) -> a[0] - b[0]);
+    Arrays.sort(slots2, (a,b) -> a[0] - b[0]);
+    int j = 0;
+    List<Integer> res = new ArrayList<>();
+    int i = 0;
+    while (i < slots1.length) {
+      if (j < slots2.length) {
+        if (slots1[i][1] <= slots2[j][1] && slots1[i][1] >= slots2[j][0]) {
+          int start = Math.max(slots2[j][0], slots1[i][0]);
+          int end = Math.min(slots2[j][1], slots1[i][1]);
+          if (end - start >= duration) {
+            res.add(start);
+            res.add(start + duration);
+            return res;
+          }
+          i++;
+        } else if (slots2[j][1] <= slots1[i][1] && slots2[j][1] >= slots1[i][0]) {
+          int start = Math.max(slots2[j][0], slots1[i][0]);
+          int end = Math.min(slots2[j][1], slots1[i][1]);
+          if (end - start >= duration) {
+            res.add(start);
+            res.add(start + duration);
+            return res;
+          }
+          j++;
+        } else {
+          if (slots1[i][1] >= slots2[j][1]) {
+            j++;
+          } else {
+            i++;
+          }
+        }
+      } else {
+        break;
+      }
+    }
+    return res;
+  }
+}
