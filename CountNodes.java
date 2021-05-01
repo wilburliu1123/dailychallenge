1b39af5e204dabed531eca5f2dfa25eda234cbe4
Wilbur Liu <wilburliu1123@gmail.com>
2021-05-01 08:37:16 -0400

    count nodes

---
+++ CountNodes.java
@@ -0,0 +1,20 @@
+public int CountNodes(TreeNode root) {
+    int res = 0;
+    if (root == null) return res;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+    int size = q.size();
+    for (int i = 0; i < size; i++) {
+    TreeNode cur = q.poll();
+    res++;
+    if (cur.left != null) {
+    q.add(cur.left);
+    }
+    if (cur.right != null) {
+    q.add(cur.right);
+    }
+    }
+    }
+    return res;
+    }
