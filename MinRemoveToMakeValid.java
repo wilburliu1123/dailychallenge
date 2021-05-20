fdf625439664bcfd2d0b0ac7d2e49e6755b0a785
Wilbur Liu <wilburliu1123@gmail.com>
2021-05-20 09:53:08 -0400

    min remove

---
+++ MinRemoveToMakeValid.java
@@ -0,0 +1,23 @@
+class MinRemoveToMakeValid {
+  public String minRemoveToMakeValid(String s) {
+    Stack<Integer> stack = new Stack<>();
+    StringBuilder sb = new StringBuilder(s);
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == '(') {
+        stack.push(i);
+      }
+      if (c == ')') {
+        if (!stack.isEmpty()) {
+          stack.pop();
+        } else {
+          sb.setCharAt(i, '*');
+        }
+      }
+    }
+    while (!stack.isEmpty()) {
+      sb.setCharAt(stack.pop(), '*');
+    }
+    return sb.toString().replaceAll("\\*", "");
+  }
+}
