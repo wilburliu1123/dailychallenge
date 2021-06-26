70de981fda715976621ef055732d25c6357f34ee
Wilbur Liu <wilburliu1123@gmail.com>
2021-06-26 09:04:36 -0400

    interval intesection

--- IntervalIntersection.java
+++ IntervalIntersection.java
@@ -20,4 +20,27 @@ class IntervalIntersection {
     }
     return res;
   }
+
+  // 6/26
+  public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+    List<int[]> list = new ArrayList<>();
+    int i = 0, j = 0;
+    while (i < firstList.length && j < secondList.length) {
+      int start = Math.max(firstList[i][0], secondList[j][0]);
+      int end = Math.min(firstList[i][1], secondList[j][1]);
+      if (start <= end) {
+        list.add(new int[] {start, end});
+      }
+      if (firstList[i][1] < secondList[j][1]) {
+        i++;
+      } else {
+        j++;
+      }
+    }
+    int[][] res = new int[list.size()][2];
+    for (int k = 0; k < res.length; k++) {
+      res[k] = list.get(k);
+    }
+    return res;
+  }
 }
